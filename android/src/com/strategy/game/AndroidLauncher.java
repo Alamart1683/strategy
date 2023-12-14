9f39e68750e8042d25a0dd5cbf57eeaa31cb7e50
Alamart1683 <Alamart1683@gmail.com>
2023-12-14 22:55:05 +0300

    Initial commit

---
+++ android/src/com/strategy/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.strategy.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.strategy.game.Strategy;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Strategy(), config);
+	}
+}
---
+++ desktop/src/com/strategy/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.strategy.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.strategy.game.Strategy;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Strategy");
+		new Lwjgl3Application(new Strategy(), config);
+	}
+}
