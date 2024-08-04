603a902ca2f8c148ca9e306853a080adb6c954c9
Alamart1683 <43591047+Alamart1683@users.noreply.github.com>
2024-08-04 22:44:03 +0300

    semi-isometric

--- core/src/com/strategy/game/map/daemon/TileChange.java
+++ core/src/com/strategy/game/map/daemon/TileChange.java
@@ -15,14 +15,14 @@ import java.util.Random;
 @Getter
 public class TileChange {
     private Season currentSeason;
-    private Map map;
-    private TextureRegion[][] tiles;
-    private Random random = new Random();
+    private final Map map;
+    private final TextureRegion[][] tiles;
+    private final Random random = new Random();
     private int currentSeasonIter;
     private TextureRegion currTile;
     private TextureRegion nextTile;
-    private TiledMapTileLayer layer;
-    ArrayList<TextureRegion> prevTiles = new ArrayList<>();
+    private final TiledMapTileLayer layer;
+    private final ArrayList<TextureRegion> prevTiles = new ArrayList<>();
 
     public TileChange(Map map, int currentSeasonIter) {
         this.map = map;
@@ -37,7 +37,7 @@ public class TileChange {
     private void setTile(TextureRegion tile, int x, int y) {
         TiledMapTileLayer.Cell cell = new TiledMapTileLayer.Cell();
         cell.setTile(new StaticTiledMapTile(tile));
-        layer.setCell(x, y, new TiledMapTileLayer.Cell().setTile(new StaticTiledMapTile(tile)));
+        layer.setCell(x, y, cell);
     }
 
     public int temperateSeasonChanging(int currentSeasonIter) {
@@ -45,54 +45,12 @@ public class TileChange {
             currentSeason = determineTemperateNextSeason();
             currentSeasonIter = 0;
         } else {
-            currTile = nextTile;
-            nextTile = determineTemperateNextTile();
+            updateCurrentTile();
             if (currentSeasonIter == 5) {
-                for (int i = 0; i < ((map.getWidth() * map.getHeight()) / 5); i++) {
-                    int x = (int) random.nextGaussian() * map.getTileWidth();
-                    int y = (int) random.nextGaussian() * map.getHeight();
-                    setTile(nextTile, x, y);
-                }
-                if (prevTiles.size() > 6) {
-                    prevTiles.add(currTile);
-                    prevTiles.remove(0);
-                }
-                else {
-                    prevTiles.add(currTile);
-                }
+                randomizeTileChanges();
             } else {
-                if (prevTiles.size() > 6) {
-                    prevTiles.add(currTile);
-                    prevTiles.remove(0);
-                }
-                else {
-                    prevTiles.add(currTile);
-                }
-                ArrayList<SuitableTerrain> addedTerrains = new ArrayList<>();
-                for (int i = 0; i < map.getWidth(); i++) {
-                    for (int j = 0; j < map.getHeight(); j++) {
-                        for (int k = prevTiles.size() - 1; k > 1; k--) {
-                            if (isNotAddedTile(addedTerrains, i, j)) {
-                                List<SuitableTerrain> suitableTerrains = determineSuitableTiles(i, j, prevTiles.get(k - 1));
-                                SuitableTerrain suitableTerrain;
-                                if (suitableTerrains.size() > 0) {
-                                    if (suitableTerrains.size() > 1)
-                                        suitableTerrain = suitableTerrains.get(random.nextInt(0, suitableTerrains.size() - 1));
-                                    else
-                                        suitableTerrain = suitableTerrains.get(0);
-                                    if (isNotAddedTile(addedTerrains, i, j)) {
-                                        setTile(prevTiles.get(k), suitableTerrain.getX(), suitableTerrain.getY());
-                                        addedTerrains.add(suitableTerrain);
-                                    }
-                                }
-                            }
-                        }
-                        if (!layer.getCell(i, j).getTile().getTextureRegion().equals(currTile) && !prevTiles.contains(layer.getCell(i, j).getTile().getTextureRegion()) ) {
-                            setTile(prevTiles.get(0), i, j);
-                            addedTerrains.add(new SuitableTerrain(prevTiles.get(0), i, j));
-                        }
-                    }
-                }
+                maintainPrevTilesList();
+                applyPrevTileChanges();
             }
             currentSeasonIter++;
         }
@@ -100,8 +58,69 @@ public class TileChange {
         return currentSeasonIter;
     }
 
+    private void updateCurrentTile() {
+        currTile = nextTile;
+        nextTile = determineTemperateNextTile();
+    }
+
+    private void randomizeTileChanges() {
+        for (int i = 0; i < ((map.getWidth() * map.getHeight()) / 5); i++) {
+            int x = (int) (random.nextGaussian() * map.getTileWidth());
+            int y = (int) (random.nextGaussian() * map.getHeight());
+            setTile(nextTile, x, y);
+        }
+        maintainPrevTilesList();
+    }
+
+    private void maintainPrevTilesList() {
+        if (prevTiles.size() > 6) {
+            prevTiles.remove(0);
+        }
+        prevTiles.add(currTile);
+    }
+
+    private void applyPrevTileChanges() {
+        ArrayList<SuitableTerrain> addedTerrains = new ArrayList<>();
+        for (int i = 0; i < map.getWidth(); i++) {
+            for (int j = 0; j < map.getHeight(); j++) {
+                applySuitableTileChanges(addedTerrains, i, j);
+                handleLayerCellChanges(addedTerrains, i, j);
+            }
+        }
+    }
+
+    private void applySuitableTileChanges(ArrayList<SuitableTerrain> addedTerrains, int i, int j) {
+        for (int k = prevTiles.size() - 1; k > 1; k--) {
+            if (isNotAddedTile(addedTerrains, i, j)) {
+                List<SuitableTerrain> suitableTerrains = determineSuitableTiles(i, j, prevTiles.get(k - 1));
+                if (!suitableTerrains.isEmpty()) {
+                    SuitableTerrain suitableTerrain = selectRandomSuitableTerrain(suitableTerrains);
+                    if (isNotAddedTile(addedTerrains, i, j)) {
+                        setTile(prevTiles.get(k), suitableTerrain.getX(), suitableTerrain.getY());
+                        addedTerrains.add(suitableTerrain);
+                    }
+                }
+            }
+        }
+    }
+
+    private void handleLayerCellChanges(ArrayList<SuitableTerrain> addedTerrains, int i, int j) {
+        if (!layer.getCell(i, j).getTile().getTextureRegion().equals(currTile) && !prevTiles.contains(layer.getCell(i, j).getTile().getTextureRegion())) {
+            setTile(prevTiles.get(0), i, j);
+            addedTerrains.add(new SuitableTerrain(prevTiles.get(0), i, j));
+        }
+    }
+
+    private SuitableTerrain selectRandomSuitableTerrain(List<SuitableTerrain> suitableTerrains) {
+        if (suitableTerrains.size() > 1) {
+            return suitableTerrains.get(random.nextInt(suitableTerrains.size() - 1));
+        } else {
+            return suitableTerrains.get(0);
+        }
+    }
+
     private boolean isNotAddedTile(ArrayList<SuitableTerrain> addedTerrains, int x, int y) {
-        for (SuitableTerrain terrain: addedTerrains) {
+        for (SuitableTerrain terrain : addedTerrains) {
             if (terrain.getX() == x && terrain.getY() == y)
                 return false;
         }
@@ -109,54 +128,27 @@ public class TileChange {
     }
 
     private TextureRegion determineTemperateNextTile() {
-        if (currentSeason == Season.Summer) {
-            if (currentSeasonIter >= 0 && currentSeasonIter < 5)
-                return tiles[0][2];
-            else if (currentSeasonIter == 5)
-                return tiles[0][3];
-            else if (currentSeasonIter == 6)
-                return tiles[0][4];
-            else if (currentSeasonIter == 7)
-                return tiles[1][0];
-            else if (currentSeasonIter == 8)
-                return tiles[1][1];
-        }
-        else if (currentSeason == Season.Autumn) {
-            if (currentSeasonIter >= 0 && currentSeasonIter < 5)
-                return tiles[1][2];
-            else if (currentSeasonIter == 5)
-                return tiles[1][3];
-            else if (currentSeasonIter == 6)
-                return tiles[1][4];
-            else if (currentSeasonIter == 7)
-                return tiles[2][0];
-            else if (currentSeasonIter == 8)
-                return tiles[2][1];
-        }
-        else if (currentSeason == Season.Winter) {
-            if (currentSeasonIter >= 0 && currentSeasonIter < 5)
-                return tiles[2][2];
-            else if (currentSeasonIter == 5)
-                return tiles[2][3];
-            else if (currentSeasonIter == 6)
-                return tiles[2][4];
-            else if (currentSeasonIter == 7)
-                return tiles[3][0];
-            else if (currentSeasonIter == 8)
-                return tiles[3][1];
-        } else if (currentSeason == Season.Spring) {
-            if (currentSeasonIter >= 0 && currentSeasonIter < 5)
-                return tiles[3][2];
-            else if (currentSeasonIter == 5)
-                return tiles[3][3];
-            else if (currentSeasonIter == 6)
-                return tiles[3][4];
-            else if (currentSeasonIter == 7)
-                return tiles[0][0];
-            else if (currentSeasonIter == 8)
-                return tiles[0][1];
-        }
+        int[][] seasonTiles = getSeasonTiles(currentSeason);
+        if (currentSeasonIter >= 0 && currentSeasonIter < 5)
+            return tiles[seasonTiles[0][0]][seasonTiles[0][1]];
+        else if (currentSeasonIter == 5)
+            return tiles[seasonTiles[1][0]][seasonTiles[1][1]];
+        else if (currentSeasonIter == 6)
+            return tiles[seasonTiles[2][0]][seasonTiles[2][1]];
+        else if (currentSeasonIter == 7)
+            return tiles[seasonTiles[3][0]][seasonTiles[3][1]];
+        else if (currentSeasonIter == 8)
+            return tiles[seasonTiles[4][0]][seasonTiles[4][1]];
         return null;
+    }
+
+    private int[][] getSeasonTiles(Season season) {
+        return switch (season) {
+            case Summer -> new int[][]{{0, 2}, {0, 3}, {0, 4}, {1, 0}, {1, 1}};
+            case Autumn -> new int[][]{{1, 2}, {1, 3}, {1, 4}, {2, 0}, {2, 1}};
+            case Winter -> new int[][]{{2, 2}, {2, 3}, {2, 4}, {3, 0}, {3, 1}};
+            case Spring -> new int[][]{{3, 2}, {3, 3}, {3, 4}, {0, 0}, {0, 1}};
+        };
     }
 
     public Season determineTemperateNextSeason() {
@@ -170,30 +162,20 @@ public class TileChange {
 
     private List<SuitableTerrain> determineSuitableTiles(int x, int y, TextureRegion prevTile) {
         List<SuitableTerrain> suitableTiles = new ArrayList<>();
-        if (x - 1 < map.getWidth() && x - 1 >= 0)
-            if (layer.getCell(x - 1, y).getTile().getTextureRegion().equals(prevTile))
-                suitableTiles.add(new SuitableTerrain(layer.getCell(x - 1, y).getTile().getTextureRegion(), x - 1, y));
-        if (x + 1 < map.getWidth())
-            if (layer.getCell(x + 1, y).getTile().getTextureRegion().equals(prevTile))
-                suitableTiles.add(new SuitableTerrain(layer.getCell(x + 1, y).getTile().getTextureRegion(), x + 1, y));
-        if (y + 1 < map.getHeight())
-            if (layer.getCell(x, y + 1).getTile().getTextureRegion().equals(prevTile))
-                suitableTiles.add(new SuitableTerrain(layer.getCell(x, y + 1).getTile().getTextureRegion(), x, y + 1));
-        if (y - 1 < map.getWidth() && y - 1 >= 0)
-            if (layer.getCell(x, y - 1).getTile().getTextureRegion().equals(prevTile))
-                suitableTiles.add(new SuitableTerrain(layer.getCell(x,y - 1).getTile().getTextureRegion(), x, y - 1));
-        if (x + 1 < map.getWidth() && y + 1 < map.getHeight())
-            if (layer.getCell(x + 1, y + 1).getTile().getTextureRegion().equals(prevTile))
-                suitableTiles.add(new SuitableTerrain(layer.getCell(x + 1, y + 1).getTile().getTextureRegion(), x + 1, y + 1));
-        if (x - 1 < map.getWidth() && x - 1 >= 0 && y + 1 < map.getHeight())
-            if (layer.getCell(x - 1, y + 1).getTile().getTextureRegion().equals(prevTile))
-                suitableTiles.add(new SuitableTerrain(layer.getCell(x - 1,y + 1).getTile().getTextureRegion(), x - 1, y + 1));
-        if (x + 1 < map.getWidth() && y - 1 < map.getHeight() && y - 1 >= 0)
-            if (layer.getCell(x + 1,y - 1).getTile().getTextureRegion().equals(prevTile))
-                suitableTiles.add(new SuitableTerrain(layer.getCell(x + 1, y - 1).getTile().getTextureRegion(), x + 1, y - 1));
-        if (x - 1 < map.getWidth() && x - 1 >= 0 && y - 1 < map.getHeight() && y - 1 >= 0)
-            if (layer.getCell(x - 1,y - 1).getTile().getTextureRegion().equals(prevTile))
-                suitableTiles.add(new SuitableTerrain(layer.getCell(x - 1,y - 1).getTile().getTextureRegion(), x - 1, y - 1));
+        addSuitableTerrain(suitableTiles, x - 1, y, prevTile);
+        addSuitableTerrain(suitableTiles, x + 1, y, prevTile);
+        addSuitableTerrain(suitableTiles, x, y + 1, prevTile);
+        addSuitableTerrain(suitableTiles, x, y - 1, prevTile);
+        addSuitableTerrain(suitableTiles, x + 1, y + 1, prevTile);
+        addSuitableTerrain(suitableTiles, x - 1, y + 1, prevTile);
+        addSuitableTerrain(suitableTiles, x + 1, y - 1, prevTile);
+        addSuitableTerrain(suitableTiles, x - 1, y - 1, prevTile);
         return suitableTiles;
+    }
+
+    private void addSuitableTerrain(List<SuitableTerrain> suitableTiles, int x, int y, TextureRegion prevTile) {
+        if (x >= 0 && x < map.getWidth() && y >= 0 && y < map.getHeight() && layer.getCell(x, y).getTile().getTextureRegion().equals(prevTile)) {
+            suitableTiles.add(new SuitableTerrain(layer.getCell(x, y).getTile().getTextureRegion(), x, y));
+        }
     }
 }
